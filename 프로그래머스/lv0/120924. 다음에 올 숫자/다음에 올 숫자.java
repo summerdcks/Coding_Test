970979dcf0eee0e5b2eec9d552e21ec583875cd7
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-07-19 17:58:32 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        if(common[1] - common[0] == common[common.length-1] - common[common.length-2]) {
+            answer = common[common.length-1] + common[1] - common[0];
+        } else {
+            answer = common[common.length-1] * common[1] / common[0];
+        }
+        return answer;
+    }
+}

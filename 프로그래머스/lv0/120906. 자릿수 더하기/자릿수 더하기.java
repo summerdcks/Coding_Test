69239fe3236ad45d2ea0815841b1d09236d15f8c
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-07-24 19:27:29 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String number = Integer.toString(n);
+        for(int i = 0; i < number.length(); i++) {
+            answer += Character.getNumericValue(number.charAt(i));
+        }
+        return answer;
+    }
+}

10cc1f388ffe6a10a6db6355ca402119b6dc00de
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-07-25 17:09:50 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0 ; i < num_list.length; i = i+n) {
+            arr.add(num_list[i]);
+        }
+        int[] answer = new int[arr.size()];
+        for(int i = 0 ; i < arr.size(); i ++) {
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}

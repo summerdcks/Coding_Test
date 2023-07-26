25f8ed809196cbe20c1fdbec99796254f0581219
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-07-26 11:19:06 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        if(direction.equals("right")) {
+            int temp = numbers[numbers.length-1];
+            for(int i = numbers.length-2; i >= 0; i--) {
+                numbers[i+1] = numbers[i];
+            }            
+            numbers[0] = temp;
+        } else {
+            int temp = numbers[0];
+            for(int i = 0; i < numbers.length-1 ; i++) {
+                numbers[i] = numbers[i+1];
+            }            
+            numbers[numbers.length-1] = temp;                                    
+        }
+        return numbers;
+    }
+}

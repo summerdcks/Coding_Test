d23a66f0f3964d312fe9084f8190b44956a0a21f
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-08-01 12:35:34 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.19 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < ingredient.length; i++) {
+            st.push(ingredient[i]);            
+            if(st.size() >= 4) {
+                if(st.get(st.size()-4) == 1 && st.get(st.size()-3) == 2 
+                   && st.get(st.size()-2) == 3 && st.get(st.size()-1) == 1) {
+                    answer++;
+                    st.pop();
+                    st.pop();
+                    st.pop();
+                    st.pop();
+                }
+            }
+        }                                
+        return answer;
+    }
+}

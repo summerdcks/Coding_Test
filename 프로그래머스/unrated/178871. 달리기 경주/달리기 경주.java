71fe81baf1a9b44daf7082cbb4a68df1bea63856
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-08-01 13:06:45 +0900

    [unrated] Title: 달리기 경주, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] players, String[] callings) {       
+        //해시맵에 등수세팅
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(int i =0; i < players.length; i++) {
+            hm.put(players[i], i);
+        }
+        
+        for(int i = 0; i < callings.length; i++){
+            int me = hm.get(callings[i]);
+            int you = hm.get(callings[i])-1;
+            
+            hm.put(players[you], me);
+            hm.put(callings[i], you);
+            
+            String temp = players[me];
+            players[me] = players[you];
+            players[you] = temp;
+        }    
+
+        return players;
+    }
+}

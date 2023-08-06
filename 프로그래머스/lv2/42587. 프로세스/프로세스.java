f42119852806ce6181de5fbad53f2aeff742f3c1
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-08-06 23:29:37 +0900

    [level 2] Title: 프로세스, Time: 0.66 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i < priorities.length; i++) {
+            pq.add(priorities[i]);
+        }
+        
+        while(!pq.isEmpty()) {
+            for(int i = 0 ; i< priorities.length; i++) {
+                if(priorities[i] == pq.peek() ) {
+                    if(i == location){
+                        answer++;
+                        return answer;
+                    } else{
+                        pq.poll();
+                        answer++;
+                    }
+                }
+            }
+            
+            
+        }
+        
+        return answer;
+    }
+}

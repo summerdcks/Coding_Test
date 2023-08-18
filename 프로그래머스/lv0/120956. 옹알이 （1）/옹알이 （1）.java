c246317e75c7ae6cccd7f9ab00f841ea1aa87429
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-08-18 15:43:14 +0900

    [level 0] Title: 옹알이 (1), Time: 0.13 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,17 +2,20 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         String[] arr = {"aya", "ye", "woo", "ma"};
+        
         for(int i = 0; i < babbling.length; i++) {
             for(int j = 0; j < arr.length; j++) {
                  babbling[i] = babbling[i].replace(arr[j], " ");
             }
             babbling[i] = babbling[i].trim();
         }
+        
         for(int h = 0; h < babbling.length; h++) {
             if(babbling[h].equals("")) {
                 answer++;
             }
         }
+        
         return answer;
     }
 }

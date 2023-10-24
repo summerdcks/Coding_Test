9968d4f8c28e8a150bd4fed2aae43658c923d7b7
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-10-24 17:36:31 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int y = scanner.nextInt();
+        System.out.println(y-(2541-1998));
+    }
+}

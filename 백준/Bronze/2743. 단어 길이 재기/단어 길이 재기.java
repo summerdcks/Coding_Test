982941b02986daa38d19234e9a4425a3dd117898
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-11-07 14:22:20 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String str = scanner.next();
+    System.out.println(str.length());
+    }
+}

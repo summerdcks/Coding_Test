333a462fc29b8f576e07902e28a0a4443353a9b1
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-11-17 09:07:27 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println((double)A/B);
+    }
+}

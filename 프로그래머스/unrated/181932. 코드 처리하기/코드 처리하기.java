e7b573902da99cce8cac59b0a6f3e34883ed755a
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-12-05 10:00:09 +0900

    [level unrated] Title: 코드 처리하기, Time: 2.57 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for(int i = 0; i <= code.length()-1; i++) {
+            if(mode == 0) {
+                if(code.charAt(i) != '1' && i % 2 == 0) {
+                    answer += code.charAt(i);
+                } else if(code.charAt(i) == '1') {
+                    mode = 1;
+                }                
+            } else if(mode == 1) {
+                if(code.charAt(i) != '1' && i % 2 != 0) {
+                    answer += code.charAt(i);
+                } else if(code.charAt(i) == '1') {
+                    mode = 0;
+                }
+            }
+        }
+        if(answer.equals("")) answer += "EMPTY";
+        return answer;
+    }
+}
